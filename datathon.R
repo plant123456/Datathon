b061b32ad7afa49146249b39bf4cbc2d21f35568
todayishazelll <114307836+todayishazelll@users.noreply.github.com>
2023-04-07 05:59:00 +0100

    Update datathon.R

--- datathon.R
+++ datathon.R
@@ -1,17 +1,21 @@
 # Are there any road properties (intersections, location types, road condition), weather conditions or times of day that have a higher incidence of fatal crashes? Can we use predictive modeling to identify these high-risk areas and suggest road repair or redesign to mitigate the risks?
 
+
+
 # Load the required library
 library(dplyr)
 
 
 gen.dat<-read.csv("crash_info_general.csv", header=T)
 
-
+# filter
 merged.dat <- subset(gen.dat, select=c(BELTED_DEATH_COUNT, BICYCLE_DEATH_COUNT, CHLDPAS_DEATH_COUNT, MCYCLE_DEATH_COUNT,
                                           NONMOTR_DEATH_COUNT, PED_DEATH_COUNT, UNB_DEATH_COUNT,
                                           WEATHER1, WEATHER2, INTERSECT_TYPE,
                                           INTERSECTION_RELATED, TIME_OF_DAY,
                                           ROAD_CONDITION))
+
+# Convert to factor variable
 merged.dat$WEATHER1 <- as.factor(merged.dat$WEATHER1)
 table(merged.dat$WEATHER1)
 class(merged.dat$WEATHER1)
@@ -24,7 +28,6 @@ merged.dat$INTERSECT_TYPE <- as.factor(merged.dat$INTERSECT_TYPE)
 table(merged.dat$INTERSECT_TYPE)
 class(merged.dat$INTERSECT_TYPE)
 
-# Convert to factor variable
 merged.dat$INTERSECTION_RELATED <- as.factor(merged.dat$INTERSECTION_RELATED)
 table(merged.dat$INTERSECTION_RELATED)
 class(merged.dat$INTERSECTION_RELATED)
@@ -33,6 +36,7 @@ merged.dat$ROAD_CONDITION <- as.factor(merged.dat$ROAD_CONDITION)
 table(merged.dat$ROAD_CONDITION)
 class(merged.dat$ROAD_CONDITION)
 
+# create a fatal predictor (binary)
 merged.dat <- merged.dat %>% 
   mutate(total_death = BELTED_DEATH_COUNT + BICYCLE_DEATH_COUNT + CHLDPAS_DEATH_COUNT + MCYCLE_DEATH_COUNT + NONMOTR_DEATH_COUNT + PED_DEATH_COUNT + UNB_DEATH_COUNT)
 
@@ -40,6 +44,10 @@ merged.dat <- merged.dat %>%
   mutate(total_death = BELTED_DEATH_COUNT + BICYCLE_DEATH_COUNT + CHLDPAS_DEATH_COUNT + MCYCLE_DEATH_COUNT +
            NONMOTR_DEATH_COUNT + PED_DEATH_COUNT + UNB_DEATH_COUNT,
          fatal = ifelse(total_death > 0, 1, 0))
+
+# set baseline
+merged.dat$INTERSECTION_RELATED <- relevel(merged.dat$INTERSECTION_RELATED,
+                                ref="N")
 
 # Fit a logistic regression model
 model1 <- glm(fatal ~ WEATHER1 + WEATHER2 + INTERSECT_TYPE + 
