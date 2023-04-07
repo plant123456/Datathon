77432683a43916d0292ccd503f720974f30b7d25
goodguybenji <103256678+goodguybenji@users.noreply.github.com>
2023-04-07 15:20:24 +0100

    updated with filtered data

--- visualise_crashes.py
+++ visualise_crashes.py
@@ -3,11 +3,21 @@ import numpy as np
 import folium
 from folium.plugins import HeatMap
 
-# Load the data
-data = pd.read_csv('dataset/Crashes/crash_info_general.csv', usecols=['DEC_LAT', 'DEC_LONG'])
-data = data.replace('NaN', np.nan)
-data.dropna(inplace=True)
-data = data.reset_index(drop=True)
+# Initialises dataframe for crashes that occured
+crashes = pd.read_csv('dataset/Crashes/crash_info_general.csv', usecols=['CRN', 'DEC_LAT', 'DEC_LONG'])
+crashes.dropna(inplace=True)
+crashes.reset_index(drop=True)
+
+# Initialises dataframe for people involved in crashes
+people = pd.read_csv('dataset/Crashes/crash_info_people.csv', usecols=['CRN', 'DVR_PED_CONDITION', 'PERSON_TYPE'])
+people.dropna(inplace=True)
+people.reset_index(drop=True)
+
+# Merges the two dataframes
+merged_df = pd.merge(crashes, people, on='CRN', how='inner')
+
+# Filters for only crashes by drivers who were not under the influence of anything (crash caused by road)
+merged_df = merged_df.query('DVR_PED_CONDITION == 1').query('PERSON_TYPE == 1').reset_index(drop=True)
 
 # Create a map of Philadelphia
 map_philadelphia = folium.Map(location=[39.9526, -75.1652], zoom_start=12, tiles='Stamen Terrain')
@@ -19,7 +29,7 @@ heatmap_layer = folium.FeatureGroup(name='Heatmap')
 map_philadelphia.add_child(heatmap_layer)
 
 # Create a list of points
-points = data[['DEC_LAT', 'DEC_LONG']].values.tolist()
+points = merged_df[['DEC_LAT', 'DEC_LONG']].values.tolist()
 
 # Add the points to the heatmap layer
 heatmap_layer.add_child(HeatMap(points, radius=15, gradient={0.2: 'white', 0.4: 'yellow', 0.6: 'orange', 1: 'red'}))
